10ad252628783d1c9c18078563d88d732d0d36bf
tarikulker-js <yahyatrkulker35@gmail.com>
2022-04-09 12:07:10 +0000

    fix bug (ProfileScreen/ProfileBody: 121-139)

--- ScreenComponents/ProfileBody.js
+++ ScreenComponents/ProfileBody.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
-import Feather from 'react-native-vector-icons/Feather';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState } from "react";
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
+import Feather from "react-native-vector-icons/Feather";
+import { useNavigation } from "@react-navigation/native";
 
 export const ProfileBody = ({
   name,
@@ -10,45 +10,48 @@ export const ProfileBody = ({
   post,
   followers,
   following,
-  user
+  user,
 }) => {
   return (
     <View>
       {accountName ? (
         <View
           style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}>
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
           <View
             style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-            }}>
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
             <Text
               style={{
                 fontSize: 18,
-                fontWeight: 'bold',
-              }}>
+                fontWeight: "bold",
+              }}
+            >
               {accountName}
             </Text>
             <Feather
               name="chevron-down"
               style={{
                 fontSize: 20,
-                color: 'black',
+                color: "black",
                 paddingHorizontal: 5,
                 opacity: 0.5,
               }}
             />
           </View>
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
             <Feather
               name="plus-square"
               style={{
                 fontSize: 25,
-                color: 'black',
+                color: "black",
                 paddingHorizontal: 15,
               }}
             />
@@ -63,19 +66,21 @@ export const ProfileBody = ({
       ) : null}
       <View
         style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-around',
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-around",
           paddingVertical: 20,
-        }}>
+        }}
+      >
         <View
           style={{
-            alignItems: 'center',
-          }}>
+            alignItems: "center",
+          }}
+        >
           <Image
             source={profileImage}
             style={{
-              resizeMode: 'cover',
+              resizeMode: "cover",
               width: 80,
               height: 80,
               borderRadius: 100,
@@ -84,69 +89,86 @@ export const ProfileBody = ({
           <Text
             style={{
               paddingVertical: 5,
-              fontWeight: 'bold',
-            }}>
+              fontWeight: "bold",
+            }}
+          >
             {name}
           </Text>
           <Text
             style={{
               paddingVertical: 5,
-              fontWeight: 'bold',
-              marginTop: -10
-            }}>
+              fontWeight: "bold",
+              marginTop: -10,
+            }}
+          >
             {user.email}
           </Text>
         </View>
-        <View style={{ alignItems: 'center' }}>
-          <Text style={{ fontWeight: 'bold', fontSize: 18 }}>{post}</Text>
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontWeight: "bold", fontSize: 18 }}>{post}</Text>
           <Text>Gönderi</Text>
         </View>
-        <View style={{ alignItems: 'center' }}>
-          <Text style={{ fontWeight: 'bold', fontSize: 18 }}>{followers}</Text>
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontWeight: "bold", fontSize: 18 }}>{followers}</Text>
           <Text>Takipçi</Text>
         </View>
-        <View style={{ alignItems: 'center' }}>
-          <Text style={{ fontWeight: 'bold', fontSize: 18 }}>{following}</Text>
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontWeight: "bold", fontSize: 18 }}>{following}</Text>
           <Text>Takip</Text>
         </View>
       </View>
 
-      <View style={{ alignItems: 'left' }}>
-        <Text style={{ paddingVertical: 2.5, marginLeft: 5, marginTop: 2 }}>{user.bio}</Text>
+      <View>
+        <Text style={{ paddingVertical: 2, marginLeft: 15, marginTop: -3 }}>
+          {user.bio}
+        </Text>
       </View>
-      
-      <View style={{ alignItems: 'left' }}>
-        <Text onPress={() => Linking.openURL(user.site)} style={{ paddingVertical: 2.5, marginLeft: 5, marginTop: 2 }}>{user.site}</Text>
+
+      <View>
+        <Text
+          onPress={() => Linking.openURL(user.site)}
+          style={{
+            paddingVertical: 2,
+            marginLeft: 15,
+            marginTop: 3,
+            color: "cyan",
+          }}
+        >
+          {user.site}
+        </Text>
       </View>
-      
+
+      {/* 121-134 buglu. */}
     </View>
   );
 };
 
-export const ProfileButtons = ({ user, userProfile, profilePic, mypics, myid }) => {
+export const ProfileButtons = ({
+  user,
+  userProfile,
+  profilePic,
+  mypics,
+  myid,
+}) => {
   //id, name, accountName, profileImage
   const navigation = useNavigation();
   const [follow, setFollow] = useState(follow);
 
   return (
     <>
-      {
-        console.log("user._id", user._id),
-        console.log("myid", myid)
-
-      }
       {user._id === myid ? (
         <View
           style={{
-            width: '100%',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-evenly',
+            width: "100%",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-evenly",
             paddingVertical: 5,
-          }}>
+          }}
+        >
           <TouchableOpacity
             onPress={() =>
-              navigation.push('EditProfile', {
+              navigation.push("EditProfile", {
                 user: user,
                 userProfile: userProfile,
                 profilePic: profilePic,
@@ -155,86 +177,94 @@ export const ProfileButtons = ({ user, userProfile, profilePic, mypics, myid }) 
               })
             }
             style={{
-              width: '100%',
-            }}>
+              width: "100%",
+            }}
+          >
             <View
               style={{
-                width: '100%',
+                width: "100%",
                 height: 35,
                 borderRadius: 5,
-                borderColor: '#DEDEDE',
+                borderColor: "#DEDEDE",
                 borderWidth: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
               <Text
                 style={{
-                  fontWeight: 'bold',
+                  fontWeight: "bold",
                   fontSize: 14,
                   letterSpacing: 1,
                   opacity: 0.8,
-                }}>
+                }}
+              >
                 Profilinizi Düzenleyin
               </Text>
             </View>
           </TouchableOpacity>
         </View>
       ) : (
+        <View
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => setFollow(!follow)}
+            style={{ width: "42%" }}
+          >
+            <View
+              style={{
+                width: "100%",
+                height: 35,
+                borderRadius: 5,
+                backgroundColor: follow ? null : "#3493D9",
+                borderWidth: follow ? 1 : 0,
+                borderColor: "#DEDEDE",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ color: follow ? "black" : "white" }}>
+                {follow ? "Takip Ediliyor" : "Takip et"}
+              </Text>
+            </View>
+          </TouchableOpacity>
           <View
             style={{
-              width: '100%',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-              alignItems: 'center',
-            }}>
-            <TouchableOpacity
-              onPress={() => setFollow(!follow)}
-              style={{ width: '42%' }}>
-              <View
-                style={{
-                  width: '100%',
-                  height: 35,
-                  borderRadius: 5,
-                  backgroundColor: follow ? null : '#3493D9',
-                  borderWidth: follow ? 1 : 0,
-                  borderColor: '#DEDEDE',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                <Text style={{ color: follow ? 'black' : 'white' }}>
-                  {follow ? 'Takip Ediliyor' : 'Takip et'}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <View
-              style={{
-                width: '42%',
-                height: 35,
-                borderWidth: 1,
-                borderColor: '#DEDEDE',
-                justifyContent: 'center',
-                alignItems: 'center',
-                borderRadius: 5,
-              }}>
-              <Text>Message</Text>
-            </View>
-            <View
-              style={{
-                width: '10%',
-                height: 35,
-                borderWidth: 1,
-                borderColor: '#DEDEDE',
-                justifyContent: 'center',
-                alignItems: 'center',
-                borderRadius: 5,
-              }}>
-              <Feather
-                name="chevron-down"
-                style={{ fontSize: 20, color: 'black' }}
-              />
-            </View>
+              width: "42%",
+              height: 35,
+              borderWidth: 1,
+              borderColor: "#DEDEDE",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 5,
+            }}
+          >
+            <Text>Message</Text>
           </View>
-        )}
+          <View
+            style={{
+              width: "10%",
+              height: 35,
+              borderWidth: 1,
+              borderColor: "#DEDEDE",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 5,
+            }}
+          >
+            <Feather
+              name="chevron-down"
+              style={{ fontSize: 20, color: "black" }}
+            />
+          </View>
+        </View>
+      )}
     </>
   );
 };
--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,24 +1,30 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList, StatusBar, ScrollView, Image } from 'react-native';
+import * as React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  StatusBar,
+  ScrollView,
+  Image,
+} from "react-native";
 
-import { ProfileBody, ProfileButtons } from '../ScreenComponents/ProfileBody';
-import Entypo from 'react-native-vector-icons/Entypo';
-import BottomTabView from '../ScreenComponents/BottomTabView';
-import LocalStorage from '@react-native-async-storage/async-storage';
-import { API_URL } from '../config.json'
-import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
-import { useDispatch, useSelector } from 'react-redux';
+import { ProfileBody, ProfileButtons } from "../ScreenComponents/ProfileBody";
+import Entypo from "react-native-vector-icons/Entypo";
+import BottomTabView from "../ScreenComponents/BottomTabView";
+import LocalStorage from "@react-native-async-storage/async-storage";
+import { API_URL } from "../config.json";
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
 
 const Profile = () => {
-
   const dispatch = useDispatch();
   const navigation = useNavigation();
   React.useEffect(() => {
-    
-    const unsubscribe = navigation.addListener('focus', () => {
+    const unsubscribe = navigation.addListener("focus", () => {
       dispatch({ type: "setPageName", payload: "Profile" });
-      
     });
 
     // Return the function to unsubscribe from the event so it gets removed on unmount
@@ -37,68 +43,62 @@ const Profile = () => {
     name: null,
     password: null,
     pic: null,
-    _id: null
-  })
+    _id: null,
+  });
   const [profilePic, setProfilePic] = React.useState("");
-  const [image, setImage] = React.useState("")
-  const [url, setUrl] = React.useState("")
+  const [image, setImage] = React.useState("");
+  const [url, setUrl] = React.useState("");
   const [mypics, setPics] = React.useState([]);
   const [jwt, setJwt] = React.useState();
   const [isLoading, setLoading] = React.useState(true);
   const [myId, setMyId] = React.useState(true);
-  
 
-  const [itemmm, setItem] = React.useState("no edited.")
+  const [itemmm, setItem] = React.useState("no edited.");
 
   React.useEffect(() => {
     setLoading(true);
 
     LocalStorage.getItem("userid").then((getedMyId) => {
       setMyId(getedMyId);
-      
+
       LocalStorage.getItem("jwt").then((getedJwt) => {
         setJwt(getedJwt);
-  
+
         fetch(`${API_URL}/mypost`, {
           type: "POST",
           headers: {
-            "Authorization": "Bearer " + getedJwt
-          }
-        }).then(res => res.json())
-          .then(result => {
+            Authorization: "Bearer " + getedJwt,
+          },
+        })
+          .then((res) => res.json())
+          .then((result) => {
             console.log("pics", result);
             setPics(result.myPosts);
-  
+
             fetch(`${API_URL}/profile`, {
               type: "POST",
               headers: {
-                "Authorization": "Bearer " + getedJwt
-              }
-            }).then(res => res.json())
-              .then(result => {
-                console.log("profile", result)
+                Authorization: "Bearer " + getedJwt,
+              },
+            })
+              .then((res) => res.json())
+              .then((result) => {
+                console.log("profile", result);
                 setUser(result.user);
-                setUserProfile(result.user)
+                setUserProfile(result.user);
                 setProfilePic(result.user.pic);
-  
+
                 setLoading(false);
-  
               })
-              .catch(err => {
-                console.log("/profile error: ", err)
-              })
-  
+              .catch((err) => {
+                console.log("/profile error: ", err);
+              });
           })
-          .catch(err => {
-            console.log("/mypost error: ", err)
-          })
-  
-  
-      })
-
+          .catch((err) => {
+            console.log("/mypost error: ", err);
+          });
+      });
     });
-
-
   }, []);
 
   for (let index = 0; index < numberofcircels; index++) {
@@ -113,112 +113,115 @@ const Profile = () => {
               borderWidth: 1,
               opacity: 0.7,
               marginHorizontal: 5,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Entypo name="plus" style={{ fontSize: 40, color: 'black' }} />
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Entypo name="plus" style={{ fontSize: 40, color: "black" }} />
           </View>
         ) : (
-            <View
-              style={{
-                width: 60,
-                height: 60,
-                borderRadius: 100,
-                backgroundColor: 'black',
-                opacity: 0.1,
-                marginHorizontal: 5,
-              }}>
-            </View>
-          )}
-      </View>,
+          <View
+            style={{
+              width: 60,
+              height: 60,
+              borderRadius: 100,
+              backgroundColor: "black",
+              opacity: 0.1,
+              marginHorizontal: 5,
+            }}
+          ></View>
+        )}
+      </View>
     );
   }
 
   const renderImages = (item) => {
     return (
-      <View key={item.item._id} style={{ flex: 1, alignItems: 'center' }}>
+      <View key={item.item._id} style={{ flex: 1, alignItems: "center" }}>
         <Image
           source={{ uri: item.item.picture }}
           style={{
             height: 100,
-            width: 100
+            width: 100,
           }}
         />
       </View>
-    )
-  }
+    );
+  };
 
   return (
     <>
-    {console.log("myId in profileScreen", myId)}
+      {console.log("myId in profileScreen", myId)}
 
-      {
-        isLoading == false && myId !== null ?
-          <>
-            <View style={{ width: '100%', height: '100%', backgroundColor: 'white' }}>
-              <View style={{ width: '100%', padding: 10 }}>
-                <ProfileBody
-                  name={user.name}
-                  accountName={user.name}
-                  profileImage={{ uri: profilePic }}
-                  followers={userProfile.followers.length}
-                  following={userProfile.following.length}
-                  post={mypics.length}
-                  user={user}
-                />
-                <ProfileButtons
-                  user={user}
-                  userProfile={userProfile}
-                  profilePic={profilePic}
-                  mypics={mypics}
-                  myid={myId}
-                />
-              </View>
-              <View>
-                <Text
-                  style={{
-                    padding: 10,
-                    letterSpacing: 1,
-                    fontSize: 14,
-                  }}>
-                  Öne Çıkan Hikayeler (Çok Yakında!)
-                </Text>
-                <ScrollView
-                  horizontal={true}
-                  showsHorizontalScrollIndicator={false}
-                  style={{
-                    paddingVertical: 5,
-                    paddingHorizontal: 10,
-                  }}>
-                  {circuls}
-                </ScrollView>
-              </View>
-              {/* GALLERY */}
-
-              <SafeAreaView style={styles.container}>
-                {
-                  userProfile !== null ?
-                    <>
-                      <FlatList
-                        horizontal={false}
-                        numColumns={3}
-                        data={mypics}
-                        keyExtractor={(index) => index._id}
-                        renderItem={renderImages}
-
-                      />
-                      {console.log("userProfile", mypics)}
-                    </> : <></>
-                }
-              </SafeAreaView>
-
+      {isLoading == false && myId !== null ? (
+        <>
+          <View
+            style={{ width: "100%", height: "100%", backgroundColor: "white" }}
+          >
+            <View style={{ width: "100%", padding: 10 }}>
+              <ProfileBody
+                name={user.name}
+                accountName={user.name}
+                profileImage={{ uri: profilePic }}
+                followers={userProfile.followers.length}
+                following={userProfile.following.length}
+                post={mypics.length}
+                user={user}
+              />
+              <ProfileButtons
+                user={user}
+                userProfile={userProfile}
+                profilePic={profilePic}
+                mypics={mypics}
+                myid={myId}
+              />
             </View>
-          </>
-          :
-          <>
-            <Text>Yükleniyor...</Text>
-          </>
-      }
+            <View>
+              <Text
+                style={{
+                  padding: 10,
+                  letterSpacing: 1,
+                  fontSize: 14,
+                }}
+              >
+                Öne Çıkan Hikayeler (Çok Yakında!)
+              </Text>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                style={{
+                  paddingVertical: 5,
+                  paddingHorizontal: 10,
+                }}
+              >
+                {circuls}
+              </ScrollView>
+            </View>
+            {/* GALLERY */}
+
+            <SafeAreaView style={styles.container}>
+              {userProfile !== null ? (
+                <>
+                  <FlatList
+                    horizontal={false}
+                    numColumns={3}
+                    data={mypics}
+                    keyExtractor={(index) => index._id}
+                    renderItem={renderImages}
+                  />
+                  {console.log("userProfile", mypics)}
+                </>
+              ) : (
+                <></>
+              )}
+            </SafeAreaView>
+          </View>
+        </>
+      ) : (
+        <>
+          <Text>Yükleniyor...</Text>
+        </>
+      )}
     </>
   );
 };
@@ -230,13 +233,13 @@ const styles = StyleSheet.create({
     marginHorizontal: 16,
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: "#f9c2ff",
     padding: 20,
     marginVertical: 8,
   },
   header: {
     fontSize: 32,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   title: {
     fontSize: 24,
